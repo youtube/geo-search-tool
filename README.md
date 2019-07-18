43e81d65ab8e00c963f42619a9eb89ddf93fe2cb
Weiqing Toh <tweiqing@google.com>
2019-07-18 22:56:28 +0800

    Update README and API key

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ geo-search-tool
 A web app that uses the YouTube Data API v3's search functionality to find videos tagged with geo-coordinates.
 
 View the live demo at http://youtube.github.io/geo-search-tool/search.html
+
+To use your own API key, create a project in Google Cloud and ensure the following services are enabled:
+ - Maps Javascript API
+ - Geocoding API
+ - YouTube Data API
--- js/search.js
+++ js/search.js
@@ -51,7 +51,7 @@ var queryFromClickSearchNotURL = false;
 var INITIAL_ZOOM_LEVEL = 11;
 var MONTH_NAMES = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 //var API_ACCESS_KEY = 'AIzaSyAEcfhZe0akd47CTYaEOWQ1bLCCbLUfVEY';
-var API_ACCESS_KEY = 'AIzaSyBuQiHS4B-axfTUpBGNeJlF2J78k962zkc';
+var API_ACCESS_KEY = 'AIzaSyCucciooXX5Cic25_SnyMjDeDBDahL5z1o';
  
 
 var CAR_REGEX = /\d{4} (?:dodge|chevy|ford|toyota|bmw|mercedes|honda|chrysler|pontiac|hyundai|audi|jeep|scion|cadillac|volks|acura|lexus|suburu|nissan|mazda|suzuki|buick|gmc|chevrolet|lincoln|infiniti|mini|hummer|porsche|volvo|land|kia|saturn|mitsubishi)/i;
